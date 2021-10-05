01f9af275a670f13af9b34a0f21008fdea38c5bf
Marvin Ren <marvin.ren@yale.edu>
2021-10-05 00:18:12 -0400

    ignore

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 
 const fs = require('fs');
 const generatePage = require('./src/page-template.js');
-const profileDataArgs = process.argv.slice(2, process.argv.length);
+
+
 const [name, github] = profileDataArgs;
 // const printProfileData = profileDataArr => {
 //     for (let i = 0; i < profileDataArr.length; i++) {
