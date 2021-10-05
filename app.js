171858d764c939a5b8ce1ae41383230a76b0e389
Marvin Ren <marvin.ren@yale.edu>
2021-10-04 22:32:40 -0400

    modularize

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
 
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
 const profileDataArgs = process.argv.slice(2, process.argv.length);
+const [name, github] = profileDataArgs;
+// const printProfileData = profileDataArr => {
+//     for (let i = 0; i < profileDataArr.length; i++) {
+//         console.log(profileDataArr[i]);
+//     }
+// };
+// printProfileData(profileDataArgs)
 
-const printProfileData = profileDataArr => {
-    for (let i = 0; i < profileDataArr.length; i++) {
-        console.log(profileDataArr[i]);
-    }
 
-    console.log("============")
-    profileDataArr.forEach((profileItem) => console.log(profileItem));
-    
-};
 
-printProfileData(profileDataArgs)
+fs.writeFile('index.html', generatePage(name, github), err => {
+    if(err) throw new Rror(err);
+
+    console.log('Portfolio complete! Check out index.html to see the output!');
+})
